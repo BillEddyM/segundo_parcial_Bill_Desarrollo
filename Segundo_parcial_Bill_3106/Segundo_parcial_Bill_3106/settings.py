c412eaa388ca878a393c3322c77bf2aba709ba12
BillEddyM <billeddydaniel2018@gmail.com>
2023-09-23 17:12:46 -0600

    Crud proveedores

--- Segundo_parcial_Bill_3106/Segundo_parcial_Bill_3106/settings.py
+++ Segundo_parcial_Bill_3106/Segundo_parcial_Bill_3106/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'proveedor',
-    #'customauthentication',
+    'customauthentication',
     'rest_framework',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'Segundo_parcial_Bill_3106.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates", BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,8 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/', BASE_DIR]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+AUTH_USER_MODEL = 'customauthentication.CustomUserAuthentication'
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'auth/login'
+LOGOUT_REDIRECT_URL = 'home'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Segundo_parcial_Bill_3106/proveedor/views.py
+++ Segundo_parcial_Bill_3106/proveedor/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Proveedor
+from .forms import ProveedorForm
+from django.urls import reverse_lazy  #PARA EL REDIRECCIONAMIENTO AUTOMATICO
+#imports necesarias para la api 
+from rest_framework import viewsets
+from .serializers import ProveedorSerializer
+
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+class ProveedorListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = Proveedor
+    template_name = 'proveedor/proveedor_list.html'
+    context_object_name = 'proveedores'
+    #login_url = '/login/'
+    permission_required = 'proveedor.view_proveedor'
+    
+
+class ProveedorCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Proveedor
+    form_class = ProveedorForm
+    template_name = 'proveedor/proveedor_form.html'
+    success_url = reverse_lazy('proveedor_list')
+    #login_url = '/login/'
+    permission_required = 'proveedor.add_proveedor'
+
+class ProveedorUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Proveedor
+    form_class = ProveedorForm
+    template_name = 'proveedor/proveedor_form.html'
+    success_url = reverse_lazy('proveedor_list')
+    #login_url = '/login/'
+    permission_required = 'proveedor.change_proveedor'
+
+class ProveedorDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Proveedor
+    template_name = 'proveedor/proveedor_confirm_delete.html' #direccion del template 
+    success_url = reverse_lazy('proveedor_list') #para redireccionar automaticamente reverse lazy 
+    #login_url = '/login/'
+    permission_required = 'proveedor.delete_proveedor'
